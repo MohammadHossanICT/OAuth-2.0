de48892d4e068ea1764873bbc6c0b1bcd6ad035c
MohammadHossan <mohammadrasel83@outlook.com>
2023-04-15 19:50:20 +0100

    Token Based authentication example

--- BasicAuthencationPartOne/BasicAuthencationPartOne/ContentView.swift
+++ BasicAuthencationPartOne/BasicAuthencationPartOne/ContentView.swift
@@ -9,49 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     @ObservedObject var viewModel: SignInViewModel
-
+    
     var body: some View {
-            HStack {
-                Spacer()
-
-                VStack {
-                    VStack(alignment: .leading) {
-                        Text("Email")
-                        TextField("Email", text: $viewModel.email)
-                            .autocapitalization(.none)
-                            .keyboardType(.emailAddress)
-                            .disableAutocorrection(true)
-                        Text("Password")
-                        SecureField("Password", text: $viewModel.password)
-                    }
-                    .textFieldStyle(.roundedBorder)
-                    .disabled(viewModel.isSigningIn)
-
-                    if viewModel.isSigningIn {
-                        ProgressView()
-                            .progressViewStyle(.circular)
-                    } else {
-                        Button("Sign In") {
-                            viewModel.signIn()
-                        }
-                    }
-
-                    Spacer()
+        HStack {
+            Spacer()
+            
+            VStack {
+                VStack(alignment: .leading) {
+                    Text("Email")
+                    TextField("Email", text: $viewModel.email)
+                        .autocapitalization(.none)
+                        .keyboardType(.emailAddress)
+                        .disableAutocorrection(true)
+                    Text("Password")
+                    SecureField("Password", text: $viewModel.password)
                 }
-                .padding()
-                .frame(maxWidth: 400.0)
-
+                .textFieldStyle(.roundedBorder)
+                .disabled(viewModel.isSigningIn)
+                
+                if viewModel.isSigningIn {
+                    ProgressView()
+                        .progressViewStyle(.circular)
+                } else {
+                    Button("Sign In") {
+                        viewModel.signIn()
+                    }
+                }
+                
                 Spacer()
             }
-            .alert(isPresented: $viewModel.hasError) {
-                Alert(
-                    title: Text("Sign In Failed"),
-                    message: Text("The email/password combination is invalid.")
-                )
-            }
+            .padding()
+            .frame(maxWidth: 400.0)
+            
+            Spacer()
         }
-
+        .alert(isPresented: $viewModel.hasError) {
+            Alert(
+                title: Text("Sign In Failed"),
+                message: Text("The email/password combination is invalid.")
+            )
+        }
     }
+    
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
